c2784628a4dfcce09125252721478085e5317df8
Junio C Hamano <gitster@pobox.com>
2020-09-08 12:25:48 -0700

    quote_path: give flags parameter to quote_path()
    The quote_path() function computes a path (relative to its base
    directory) and c-quotes the result if necessary.  Teach it to take a
    flags parameter to allow its behaviour to be enriched later.
    
    No behaviour change intended.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- quote.c
+++ quote.c
@@ -352,12 +352,12 @@ void write_name_quoted_relative(const char *name, const char *prefix,
 }
 
 /* quote path as relative to the given prefix */
-char *quote_path(const char *in, const char *prefix, struct strbuf *out)
+char *quote_path(const char *in, const char *prefix, struct strbuf *out, unsigned flags)
 {
 	struct strbuf sb = STRBUF_INIT;
 	const char *rel = relative_path(in, prefix, &sb);
 	strbuf_reset(out);
-	quote_c_style_counted(rel, strlen(rel), out, NULL, 0);
+	quote_c_style_counted(rel, strlen(rel), out, NULL, flags);
 	strbuf_release(&sb);
 
 	return out->buf;
